d90c14d082041ab3a6a2bb259c88f46813757b57
AleksSPb <paraleksi@gmail.com>
2015-09-07 19:51:50 +0300

    Java configuration

---
+++ src/main/java/com/springapp/mvc/config/AppInit.java
@@ -0,0 +1,26 @@
+package com.springapp.mvc.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+/**
+ * Created by aleks on 07.09.2015.
+ */
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer{
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] {
+                WebConfig.class
+        };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] {
+                WebConfig.class
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
