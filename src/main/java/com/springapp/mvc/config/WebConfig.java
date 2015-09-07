dadb4a60abbb483babc55a163d1d42b382c6d49a
AleksSPb <paraleksi@gmail.com>
2015-09-08 01:09:11 +0300

    Исправлены замечания по комментариям, лишнему коду и т.п.

--- src/main/java/com/springapp/mvc/config/WebConfig.java
+++ src/main/java/com/springapp/mvc/config/WebConfig.java
@@ -9,27 +9,18 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
-/**
- * Created by aleks on 07.09.2015.
- */
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.springapp.mvc")
 public class WebConfig extends WebMvcConfigurerAdapter{
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry)
-    {
-        registry.addResourceHandler("/WEB-INF/pages/**").addResourceLocations("/pages/");
-    }
-
     @Bean
     public  InternalResourceViewResolver setupViewResolver() {
         InternalResourceViewResolver resolver=new InternalResourceViewResolver();
         resolver.setPrefix("/WEB-INF/pages/");
         resolver.setSuffix(".jsp");
-        resolver.setViewClass(JstlView.class);
+        //resolver.setViewClass(JstlView.class);
         return resolver;
     }
 
