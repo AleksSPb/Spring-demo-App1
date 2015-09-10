07a9a0b64b803fa357150e028fec53f3fa68b251
AleksSPb <paraleksi@gmail.com>
2015-09-10 20:14:38 +0300

    add files

---
+++ src/main/resources/db/create-db.sql
@@ -0,0 +1,7 @@
+--DROP TABLE users IF EXISTS;
+
+CREATE TABLE peoples (
+  id         INTEGER PRIMARY KEY auto_increment,
+  name VARCHAR(50),
+  age INTEGER
+);
---
+++ src/main/resources/db/insert-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO peoples ( "NAME", "AGE" ) VALUES ('mkyong', 56);
+INSERT INTO peoples ( "NAME", "AGE" ) VALUES ('aleksSpb', 35);
+INSERT INTO peoples ( "NAME", "AGE" ) VALUES ('Vlad', 14);
