0a5e920d553c0259c06e2f0396999ac893dc2841
AleksSPb <paraleksi@gmail.com>
2015-09-11 20:37:49 +0300

    0.7.3.2 Удалены файлы создания и заполнения БД

--- src/main/resources/db/create-db.sql
+++
@@ -1,7 +0,0 @@
---DROP TABLE users IF EXISTS;
-
-CREATE TABLE peoples (
-  id         INTEGER PRIMARY KEY auto_increment,
-  name VARCHAR(50),
-  age INTEGER
-);
--- src/main/resources/db/insert-data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO peoples ( "NAME", "AGE" ) VALUES ('mkyong', 56);
-INSERT INTO peoples ( "NAME", "AGE" ) VALUES ('aleksSpb', 35);
-INSERT INTO peoples ( "NAME", "AGE" ) VALUES ('Vlad', 14);
