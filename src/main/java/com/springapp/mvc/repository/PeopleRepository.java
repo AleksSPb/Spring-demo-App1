d3320cf4ff7a9e9a6b2e01b4a3ca132afb4ff665
AleksSPb <paraleksi@gmail.com>
2015-09-23 19:37:20 +0300

    Удалил закомментированные фрагменты, неиспользуемые импорты

--- src/main/java/com/springapp/mvc/repository/PeopleRepository.java
+++ src/main/java/com/springapp/mvc/repository/PeopleRepository.java
@@ -3,7 +3,6 @@ package com.springapp.mvc.repository;
 import com.springapp.mvc.model.People;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.transaction.annotation.Transactional;
 
 public interface PeopleRepository extends JpaRepository<People, Integer> {
 
